50be10ca69be832867464930446f90f3ddb934ab
Ariadna L <kriadne1@gmail.com>
2024-08-20 17:38:13 -0600

    prueba

---
+++ numpalindromo.py
@@ -0,0 +1,9 @@
+class Solution():
+    def isPalindrome(self, x):
+        x = str(x)    
+        if x == x[::-1]:
+            return True
+        else: return False
+    
+Hola=Solution()
+Solution.isPalindrome()
