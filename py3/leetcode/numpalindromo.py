0460e1284f513523ade276a0cf085fd26391e1e0
kriadne <kriadne1@gmail.com>
2024-08-20 17:39:14 -0600

    Delete py3/leetcode directory

--- py3/leetcode/numpalindromo.py
+++
@@ -1,9 +0,0 @@
-class Solution():
-    def isPalindrome(self, x):
-        x = str(x)    
-        if x == x[::-1]:
-            return True
-        else: return False
-    
-Hola=Solution()
-Solution.isPalindrome()
